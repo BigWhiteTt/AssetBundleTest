88424aa49fa30f730dbb8fbef421c165c15a9fb7
15736873358@163.com <15736873358@163.com>
2019-07-26 10:27:06 +0800

    简单的assetbundle测试

---
+++ AssetBundleTest/Assets/Editor/BuildBundle.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using System.IO;
+
+public class BuildBundle : MonoBehaviour {
+
+    [MenuItem("MyMenu/Build Assetbundle")]
+    static private void BuildAssetBundle()
+    {
+        string dir = Application.dataPath + "/StreamingAssets";
+
+        if (!Directory.Exists(dir))
+        {
+            Directory.CreateDirectory(dir);
+        }
+        //DirectoryInfo rootDirInfo = new DirectoryInfo(Application.dataPath + "/test_atlas");
+        //List<Sprite> assets = new List<Sprite>();
+        //foreach (FileInfo pngFile in rootDirInfo.GetFiles("*.png", SearchOption.AllDirectories))
+        //{
+        //    string allPath = pngFile.FullName;
+        //    string assetPath = allPath.Substring(allPath.IndexOf("Assets"));
+        //    assets.Add(AssetDatabase.LoadAssetAtPath<Sprite>(assetPath));
+        //}
+        BuildPipeline.BuildAssetBundles(dir, BuildAssetBundleOptions.UncompressedAssetBundle, GetBuildTarget());
+        
+    }
+    static private BuildTarget GetBuildTarget()
+    {
+        BuildTarget target = new BuildTarget();
+#if UNITY_STANDALONE
+        target = BuildTarget.StandaloneWindows;
+#elif UNITY_IPHONE
+			target = BuildTarget.iPhone;
+#elif UNITY_ANDROID
+			target = BuildTarget.Android;
+#endif
+        return target;
+    }
+}
---
+++ AssetBundleTest/Assets/UIMain.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class UIMain : MonoBehaviour
+{
+    AssetBundle assetbundle = null;
+    AssetBundle assetbundle1 = null;
+    AssetBundle assetbundle2 = null;
+    GameObject go1;
+    GameObject go2;
+    GameObject go3;
+    GameObject go4;
+    void Start()
+    {
+        go1 = CreatImage(loadSprite("test_1"));
+        go2 = CreatImage(loadSprite("test_2"));
+        assetbundle1 = loadGameobject("prefanone");
+        go3 = Instantiate(assetbundle1.LoadAsset("prefab1") as GameObject);
+        go3.transform.parent = transform;
+        assetbundle2 = loadGameobject("prefabtwo");
+        go4 = Instantiate(assetbundle2.LoadAsset("prefab2") as GameObject);
+        go4.transform.parent = transform;
+        Invoke("DestoryImage", 3);
+    }
+
+    private void DestoryImage()
+    {
+        Destroy(go1);
+        Destroy(go2);
+        Destroy(go3);
+        Destroy(go4);
+        assetbundle.Unload(false);
+        assetbundle1.Unload(false);
+        assetbundle2.Unload(false);
+        Resources.UnloadUnusedAssets();
+    }
+
+    private GameObject CreatImage(Sprite sprite)
+    {
+        GameObject go = new GameObject(sprite.name);
+        go.layer = LayerMask.NameToLayer("UI");
+        go.transform.parent = transform;
+        go.transform.localScale = Vector3.one;
+        Image image = go.AddComponent<Image>();
+        image.sprite = sprite;
+        image.SetNativeSize();
+        return go;
+    }
+
+    private Sprite loadSprite(string spriteName)
+    {
+        if (assetbundle == null)
+            assetbundle = AssetBundle.LoadFromFile(Application.streamingAssetsPath + "/test.u3d");
+        return assetbundle.LoadAsset(spriteName) as Sprite;
+    }
+
+    private AssetBundle loadGameobject(string bundlename)
+    {
+        AssetBundle asset = AssetBundle.LoadFromFile(Application.streamingAssetsPath + "/" + bundlename + ".u3d");
+        return asset;
+    }
+
+}
